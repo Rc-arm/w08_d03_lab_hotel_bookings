bdb68dd2c60c976c8684564d91f02c9317cc88cc
Rob Armitage <robert.c.armitage@gmail.com>
2019-09-18 16:02:15 +0100

    update CSS

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="app">
-    <h1>My Hotel App</h1>
-    <bookings-form />
-    <bookings-grid :bookings="bookings" />
+    <section id="top-of-page">
+      <h1 id="page-header">Really Good Hotel</h1>
+      <h2 id="subtitle-1">Come and stay with Ian and Rob. You'll be fine.</h2>
+      <h3 id="subtitle-2">Trust us.</h3>
+      <img id="quality" src="./assets/premium-quality-rubber-stamp.jpg">
+    </section>
+    <div id="flex-container">
+      <bookings-form id="bookings-form"/>
+      <bookings-grid :bookings="bookings" id="bookings-grid"/>
+    </div>
   </div>
 </template>
 
@@ -51,4 +58,47 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#flex-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+#bookings-form {
+  text-align: left;
+}
+
+#bookings-grid {
+  text-align: left;
+}
+
+#top-of-page {
+  background-color: royalblue;
+  margin: 2em;
+  color: #D2BA19;
+
+}
+
+#page-header {
+  font-size: 3em;
+  padding-top: 0.3em;
+}
+
+#subtitle-1 {
+  font-size: 1.5em;
+  margin: 0.5em;
+}
+
+#subtitle-2 {
+  font-size: 0.8em;
+  margin: 0.5em;
+  padding-bottom: 1em;
+}
+
+#quality {
+  height: 7em;
+  float: left;
+}
+
 </style>
--- client/src/components/BookingsGrid.vue
+++ client/src/components/BookingsGrid.vue
@@ -1,11 +1,23 @@
 <template lang="html">
-  <div id="bookingsGrid">
+  <div>
     <div class="booking" v-for="booking in bookings">
-      <h2>{{ booking.name }}</h2>
-      <p>{{ booking.email }}</p>
-      <p>{{ booking.status }}</p>
-
-      <button v-on:click="deleteBooking(booking._id)">Delete Booking</button>
+      <h3>Booking</h3>
+      <ul>
+        <li>Name: {{ booking.name }}</li>
+        <li>Email: {{ booking.email }}</li>
+        <li>Status: {{ booking.status }}</li>
+      </ul>
+      <button id="delete-button" v-on:click="deleteBooking(booking._id)">Delete Booking</button>
+      <!-- <li v-for="(item, index) in items"
+          v-bind:class="{'purchased': item.purchased, 'notPurchased': !item.purchased}">
+        <span>{{ item.name }}</span>
+        <span v-if="item.purchased">Purchased!</span>
+        <button v-if="!item.purchased"
+                v-on:click="buyItem(index)">
+          Purchase
+        </button> -->
+      <button id="status-button" v-if="!booking.status" v-on:click="updateBooking(booking.status)">Check In</button>
+      <!-- <input type="checkbox" v-model="status"> -->
     </div>
   </div>
 </template>
@@ -21,7 +33,8 @@ export default {
     deleteBooking(id) {
       BookingService.deleteBooking(id)
       .then(() => eventBus.$emit('booking-deleted', id))
-    }
+    },
+
   }
 }
 </script>
